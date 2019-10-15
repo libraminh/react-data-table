ff5f585d899343ad96f450a09381dbb34d8eaf8d
Minh Le <minh.le@construct.sg>
2019-10-15 13:06:42 +0700

    [master] upload files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
+import ReactTable from 'react-table';
+import 'react-table/react-table.css'
+
 function App() {
+  const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetchPosts();
+  }, [])
+
+  const fetchPosts = async () => {
+    try {
+      let url = 'https://jsonplaceholder.typicode.com/posts';
+  
+      const postsRes = await fetch(url);
+      const posts = await postsRes.json();
+
+      setPosts(posts);
+      setLoading(false);
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  const columns = [
+    {
+      Header: 'ID',
+      accessor: 'id', // String-based value accessors!
+      width: 50,
+    },
+    {
+      Header: 'UserId',
+      accessor: 'userId',
+      width: 100,
+    },
+    {
+      Header: 'Title',
+      accessor: 'title',
+      sortable: false,
+    },
+    {
+      Header: 'Body',
+      accessor: 'body',
+      sortable: false,
+    },
+  ]
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ReactTable
+        data={posts}
+        columns={columns}
+        loading={loading}
+        filterable
+      />
     </div>
   );
 }
